03f243cdcf30d0de11dd9d55ab67cc20df1ead1c
marcopiraccini <marco.piraccini@gmail.com>
2021-03-11 07:00:06 +0100

    removed discord

--- vhserverbackup.sh
+++ vhserverbackup.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-discordwebhook="https://discord.com/api/webhooks/XXXXXXXXXXXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXX_XXXXXXXXXXXXXXXXXXXXXX"
-worldname="ENTERWORLDNAMEHERE"
+worldname="Cialthronesgard"
 now=$(date +"%d-%m-%Y")
-
-
-# Sending Stopping notifications
-
-/usr/local/sbin/discord.sh --webhook-url="${discordwebhook}" --text "@everyone ATTENTION: Valheim server will stop in 10 minutes for backup. Please log out!"
-
-sleep 5m
-
-/usr/local/sbin/discord.sh --webhook-url="${discordwebhook}" --text "@everyone ATTENTION: Valheim server will stop in 5 minutes for backup. Please log out!"
-
-sleep 4m
-
-/usr/local/sbin/discord.sh --webhook-url="${discordwebhook}" --text "@everyone ATTENTION: Valheim server stops in 1 minute for backup. Log out NOW!"
-
-sleep 1m
-
-/usr/local/sbin/discord.sh --webhook-url="${discordwebhook}" --text "Valheim Server stops now!"
 
 # Stop the Service
 systemctl stop valheimserver.service
@@ -31,11 +13,10 @@ mkdir /home/steam/backups/${now}
 
 # copy the files
 cp /home/steam/.config/unity3d/IronGate/Valheim/worlds/${worldname}.db /home/steam/backups/${now}/${worldname}.db
-
 cp /home/steam/.config/unity3d/IronGate/Valheim/worlds/${worldname}.fwl /home/steam/backups/${now}/${worldname}.fwl
 
 # clear backups older than 30 days
-/usr/local/sbin/vhclearbackups.sh
+# /usr/local/sbin/vhclearbackups.sh
 
 # Start the Service
 systemctl start valheimserver.service
